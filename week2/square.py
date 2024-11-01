7bedf73bbe9509fde48cd907389a39a2576176cb
RizvanaKA <richuu964@gmail.com>
2024-11-01 16:24:18 +0530

    Add files via upload

---
+++ week2/square.py
@@ -0,0 +1,9 @@
+n=int(input("enter the nth number in the series:"))
+
+i=1
+
+print("the squares of the first" , n , "numbers is:")
+
+while(i <= n):
+    print(i , "*" , i , "=", (i*i) )
+    i += 1
