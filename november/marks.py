73e9645fb8a30ea3fe329e0bd9f207c7a6e9c795
RizvanaKA <richuu964@gmail.com>
2024-12-12 14:51:39 +0530

    Add files via upload

---
+++ november/marks.py
@@ -0,0 +1,13 @@
+print("Enter marks of five subjects: ")
+S1=int(input())
+S2=int(input())
+S3=int(input())
+S4=int(input())
+S5=int(input())
+
+total = S1 + S2 + S3 + S4 + S5;
+percentage = (total / 500.0) * 100;
+
+
+print("Total marks = ", total)
+print("Percentage = ", percentage)
